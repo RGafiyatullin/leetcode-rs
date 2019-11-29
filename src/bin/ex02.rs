fce4eaf65cd369ec47de141966f7c0278e15e9c3
Roman Gafiyatullin <r.gafiyatullin@me.com>
2019-11-29 23:46:56 +0300

    checkpoint

--- src/bin/ex02.rs
+++ src/bin/ex02.rs
@@ -1,145 +1,103 @@
 fn main() -> () {
     let stdin = std::io::stdin();
+
     let mut input = String::new();
     let mut pattern = String::new();
+
     stdin.read_line(&mut input).unwrap();
     stdin.read_line(&mut pattern).unwrap();
-    let pattern = Pattern::from(pattern.trim());
-    println!("pattern: {:?}", pattern);
-    println!("{:?}", pattern.is_match(&input.trim()));
+
+    let input = input.trim();
+    let pattern = pattern.trim();
+
+    println!(
+        "{:?}",
+        Solution::is_match(input.to_owned(), pattern.to_owned())
+    );
 }
 
 struct Solution;
 
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
-        let pattern = Pattern::from(&p);
+        use ex02::*;
+
+        let pattern: Pattern = p.parse().expect("failed to parse the pattern");
         pattern.is_match(&s)
     }
 }
 
-trait CharMatch {
-    fn is_match(&self, ch: char) -> bool;
-}
-impl CharMatch for Option<char> {
-    fn is_match(&self, ch: char) -> bool {
-        self.is_none() || *self == Some(ch)
-    }
-}
+mod ex02 {
+    pub use pattern::Pattern;
+    pub use str_view::StrView;
 
-#[derive(Debug)]
-enum Node {
-    Char(Option<char>),
-    Asterisk(Option<char>),
-}
-
-#[derive(Debug)]
-struct Pattern {
-    nodes: Vec<Node>,
-}
-
-impl Pattern {
-    fn is_match(&self, input: &str) -> bool {
-        let mut nodes = self.nodes.iter();
-        let mut chars = input.chars();
-
-        let mut state = State::Empty;
-
-        loop {
-            match state {
-                State::Empty => {
-                    if let Some(node) = nodes.next() {
-                        state = State::ExpectChar(node);
-                        continue;
-                    } else {
-                        return chars.next().is_none();
-                    }
-                }
-                State::ExpectChar(node) => {
-                    if let Some(char) = chars.next() {
-                        match node {
-                            Node::Char(expected) => {
-                                if expected.is_match(char) {
-                                    state = State::Empty;
-                                    continue;
-                                } else {
-                                    return false;
-                                }
-                            }
-                            Node::Asterisk(expected) => {
-                                if expected.is_match(char) {
-                                    state = State::ExpectChar(node);
-                                    continue;
-                                } else {
-                                    state = State::ExpectNode(char);
-                                    continue;
-                                }
-                            }
-                        }
-                    } else {
-                        match node {
-                            Node::Asterisk(_) => return nodes.next().is_none(),
-                            Node::Char(_) => return false,
-                        }
-                    }
-                }
-                State::ExpectNode(char) => {
-                    if let Some(node) = nodes.next() {
-                        match node {
-                            Node::Char(expected) => {
-                                if expected.is_match(char) {
-                                    state = State::Empty;
-                                    continue;
-                                } else {
-                                    return false;
-                                }
-                            }
-                            Node::Asterisk(expected) => {
-                                if expected.is_match(char) {
-                                    state = State::ExpectChar(node);
-                                    continue;
-                                } else {
-                                    state = State::ExpectNode(char);
-                                    continue;
-                                }
-                            }
-                        }
-                    } else {
-                        return false;
-                    }
-                }
+    mod str_view {
+        pub struct StrView<'a> {
+            slice: &'a str,
+            pos: usize,
+        }
+        impl<'a> StrView<'a> {
+            pub fn from_str_slice(slice: &'a str) -> StrView<'a> {
+                Self { slice, pos: 0 }
             }
         }
     }
-}
 
-#[derive(Debug)]
-enum State<'a> {
-    Empty,
-    ExpectChar(&'a Node),
-    ExpectNode(char),
-}
-
-impl<S> From<S> for Pattern
-where
-    S: AsRef<str>,
-{
-    fn from(s: S) -> Self {
-        let s: &str = s.as_ref();
-        let mut nodes: Vec<Node> = Vec::with_capacity(s.len());
-        for char in s.chars() {
-            if char == '*' {
-                if let Some(Node::Char(ch)) = nodes.pop() {
-                    nodes.push(Node::Asterisk(ch));
-                } else {
-                    panic!("Asterisk must follow a regular character")
+    mod pattern {
+        #[derive(Debug)]
+        enum Char {
+            Any,
+            Exact(char),
+        }
+        impl Char {
+            fn is_match(&self, input: char) -> bool {
+                match *self {
+                    Self::Any => true,
+                    Self::Exact(expected) => expected == input,
                 }
-            } else if char == '.' {
-                nodes.push(Node::Char(None));
-            } else {
-                nodes.push(Node::Char(Some(char)));
             }
         }
-        Self { nodes }
+
+        #[derive(Debug)]
+        enum Quantifier {
+            One,
+            Any,
+        }
+
+        #[derive(Debug)]
+        struct Node(Char, Quantifier);
+
+        #[derive(Debug)]
+        pub struct Pattern(Vec<Node>);
+
+        impl std::str::FromStr for Pattern {
+            type Err = String;
+
+            fn from_str(s: &str) -> Result<Self, Self::Err> {
+                let mut acc = Vec::<Node>::with_capacity(s.len());
+
+                for ch in s.chars() {
+                    match ch {
+                        '.' => acc.push(Node(Char::Any, Quantifier::One)),
+                        '*' => match acc.pop() {
+                            None => return Err("asterisk cannot start an expression".to_owned()),
+                            Some(Node(ch, Quantifier::Any)) => {
+                                return Err("cannot have two asterisks in a row".to_owned())
+                            }
+                            Some(Node(ch, Quantifier::One)) => acc.push(Node(ch, Quantifier::Any)),
+                        },
+                        ch => acc.push(Node(Char::Exact(ch), Quantifier::One)),
+                    }
+                }
+
+                Ok(Self(acc))
+            }
+        }
+
+        impl Pattern {
+            pub fn is_match(&self, input: &str) -> bool {
+                unimplemented!()
+            }
+        }
     }
 }
