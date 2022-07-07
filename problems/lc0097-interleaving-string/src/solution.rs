c48a62b89d04fb6cdab322a9830d849bfa29afa1
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-07 15:26:29 +0300

    LC0097 works

--- problems/lc0097-interleaving-string/src/solution.rs
+++ problems/lc0097-interleaving-string/src/solution.rs
@@ -1,24 +1,28 @@
 pub struct Solution;
 
+use std::{collections::HashSet, hash::Hash};
+
 impl Solution {
     pub fn is_interleave(left: String, right: String, target: String) -> bool {
-        left.chars().for_each(|c| assert!(c.is_ascii_alphabetic()));
-        right.chars().for_each(|c| assert!(c.is_ascii_alphabetic()));
-        target
-            .chars()
-            .for_each(|c| assert!(c.is_ascii_alphabetic()));
+        // left.chars().for_each(|c| assert!(c.is_ascii_alphabetic()));
+        // right.chars().for_each(|c| assert!(c.is_ascii_alphabetic()));
+        // target
+        //     .chars()
+        //     .for_each(|c| assert!(c.is_ascii_alphabetic()));
 
         let left = left.as_bytes();
         let right = right.as_bytes();
         let target = target.as_bytes();
 
-        let mut candidates: Vec<_> = vec![Candidate {
+        let mut candidates = CandidatePool::new();
+        candidates.extend(std::iter::once(Candidate {
             src: [Cursor::new(left), Cursor::new(right)],
             target: Cursor::new(target),
-        }];
-
+        }));
+        
         let mut found = false;
-        while let Some(candidate) = candidates.pop() {
+        while let Some(candidate) = candidates.next() {
+            // eprintln!("candidate: {}", candidate);
             if candidate.check(&mut candidates) {
                 found = true;
                 break;
@@ -29,25 +33,25 @@ impl Solution {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Eq)]
 struct Cursor<'a> {
     src: &'a [u8],
-    cursor: usize,
+    pos: usize,
 }
 impl<'a> Cursor<'a> {
     fn new(src: &'a [u8]) -> Self {
-        Self { src, cursor: 0 }
+        Self { src, pos: 0 }
     }
     fn finished(&self) -> bool {
-        self.cursor >= self.src.len()
+        self.pos >= self.src.len()
     }
     fn current(&self) -> Option<u8> {
-        self.src.get(self.cursor).copied()
+        self.src.get(self.pos).copied()
     }
     fn advance(&self) -> Self {
         if !self.finished() {
             Self {
-                cursor: self.cursor + 1,
+                pos: self.pos + 1,
                 ..(*self)
             }
         } else {
@@ -56,7 +60,7 @@ impl<'a> Cursor<'a> {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 struct Candidate<'a, const I: usize> {
     src: [Cursor<'a>; I],
     target: Cursor<'a>,
@@ -86,3 +90,65 @@ impl<'a, const I: usize> Candidate<'a, I> {
         }
     }
 }
+
+#[derive(Default, Debug, Clone)]
+struct CandidatePool<T> {
+    set: HashSet<T>,
+    pool: Vec<T>,
+}
+
+impl<T> Iterator for CandidatePool<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pool.pop()
+    }
+}
+
+impl<T> CandidatePool<T> {
+    pub fn new() -> Self {
+        Self {
+            set: Default::default(),
+            pool: Default::default(),
+        }
+    }
+}
+
+impl<T> Extend<T> for CandidatePool<T> 
+where T: Hash + Eq + Clone
+{
+    fn extend<I: IntoIterator<Item = T>>(&mut self, iter: I) {
+        for item in iter {
+            if !self.set.contains(&item) {
+                self.pool.push(item.to_owned());
+                self.set.insert(item);
+            }
+        }
+    }
+}
+
+impl PartialEq for Cursor<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.pos == other.pos
+    }
+}
+
+impl Hash for Cursor<'_> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.pos.hash(state);
+    }
+}
+
+
+
+
+
+impl<'a, const I: usize> std::fmt::Display for Candidate<'a, I> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Candidate[target: {}; src: ", self.target.pos)?;
+        self.src.iter().fold(&mut f.debug_list(), |l, s| l.entry(&s.pos)).finish()?;
+        write!(f, "]")?;
+
+        Ok(())
+    }
+}
+
--- problems/lc0097-interleaving-string/src/tests.rs
+++ problems/lc0097-interleaving-string/src/tests.rs
@@ -4,6 +4,13 @@ const CASES: &[(&str, &str, &str, bool)] = &[
     ("", "", "", true),
     ("aabcc", "dbbca", "aadbbcbcac", true),
     ("aabcc", "dbbca", "aadbbbaccc", false),
+
+    (
+        "bbbbbabbbbabaababaaaabbababbaaabbabbaaabaaaaababbbababbbbbabbbbababbabaabababbbaabababababbbaaababaa",
+        "babaaaabbababbbabbbbaabaabbaabbbbaabaaabaababaaaabaaabbaaabaaaabaabaabbbbbbbbbbbabaaabbababbabbabaab",
+        "babbbabbbaaabbababbbbababaabbabaabaaabbbbabbbaaabbbaaaaabbbbaabbaaabababbaaaaaabababbababaababbababbbababbbbaaaabaabbabbaaaaabbabbaaaabbbaabaaabaababaababbaaabbbbbabbbbaabbabaabbbbabaaabbababbabbabbab",
+        false,
+    )
 ];
 
 #[test]
@@ -16,6 +23,21 @@ fn all_cases() {
 #[test]
 fn case_0() {
     run_one_case(CASES[0]);
+}
+
+#[test]
+fn case_1() {
+    run_one_case(CASES[1]);
+}
+
+#[test]
+fn case_2() {
+    run_one_case(CASES[2]);
+}
+
+#[test]
+fn case_3() {
+    run_one_case(CASES[3]);
 }
 
 fn run_one_case(case: (&str, &str, &str, bool)) {
