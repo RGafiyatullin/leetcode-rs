a4ebac9806ced64bc26ee12920ac8d0fae898e4f
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-09 20:26:30 +0300

    LC1696: time limit exceeded (stairs.len = 89923; max_step = 16309)

--- problems/lc1696-jump-game-vi/src/solution.rs
+++ problems/lc1696-jump-game-vi/src/solution.rs
@@ -1,30 +1,33 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn min_cost_climbing_stairs(cost: Vec<i32>) -> i32 {
-        assert!(cost.len() >= 2);
-        assert!(cost.len() <= 1000);
+    pub fn max_result(cost: Vec<i32>, k: i32) -> i32 {
+        assert!(cost.len() >= 1);
+        assert!(cost.len() <= 100000);
+        assert!(k >= 1);
+        assert!(k <= 100000);
 
-        let max_step_len: usize = 2;
+        let max_step_len: usize = k as usize;
+
         let mut memo = Memo::new(vec![Option::<i32>::None; max_step_len + 1]);
 
-        for step_cost in cost.iter().rev().copied() {
+        for step_value in cost.iter().rev().copied().skip(1) {
             memo.scroll_back();
 
-            let mut min_cost = Option::<i32>::None;
+            let mut max_step_value = Option::<i32>::None;
 
             for steps_back in 1..=max_step_len {
-                let candidate = memo.get(steps_back).unwrap_or(0) + step_cost;
-                min_cost = Some(std::cmp::min(candidate, min_cost.unwrap_or(candidate)));
+                let candidate = memo.get(steps_back).unwrap_or(0) + step_value;
+                max_step_value = Some(std::cmp::max(
+                    candidate,
+                    max_step_value.unwrap_or(candidate),
+                ));
             }
 
-            *memo.get_mut(0) = min_cost;
+            *memo.get_mut(0) = max_step_value;
         }
 
-        (0..max_step_len)
-            .map(|n| memo.get(n).unwrap())
-            .min()
-            .unwrap()
+        memo.get(0).expect("cost.len() >= 1. QED") + cost.last().expect("cost.len() >= 1. QED")
     }
 }
 
