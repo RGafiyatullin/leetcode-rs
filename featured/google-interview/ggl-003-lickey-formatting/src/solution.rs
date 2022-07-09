ad5d91f84854240b5c87f0588659c54f0d237acc
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-10 01:37:59 +0300

    GGL-003 Lickey Formatting

--- featured/google-interview/ggl-003-lickey-formatting/src/solution.rs
+++ featured/google-interview/ggl-003-lickey-formatting/src/solution.rs
@@ -2,12 +2,29 @@ pub struct Solution;
 
 impl Solution {
     pub fn license_key_formatting(s: String, k: i32) -> String {
-        let rev_chars = s.chars()
+        let k = k as usize;
+        let mut out = s.chars()
             .rev()
             .filter(|ch| ch.is_alphanumeric())
-            .map(|ch| ch.is_ascii_uppercase());
+            .map(|ch| ch.to_ascii_uppercase())
+            .enumerate()
+            .fold(vec![], |mut acc, (idx, ch)| {
+                acc.push(ch);
+
+                if idx % k == k - 1 {
+                    acc.push('-');
+                }
+
+                acc
+            });
+
         
-        unimplemented!()
+        if let Some('-') = out.last() {
+            out.pop();
+        }
+        out.reverse();
+
+        out.into_iter().collect()
     }
 }
 
