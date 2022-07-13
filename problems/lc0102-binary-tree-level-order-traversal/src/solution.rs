1e0d0e121541cdfedeb8c8aa4c07617d9b07dcf4
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-13 20:03:29 +0300

    LC0102 Binary Tree Level Order Traversal

---
+++ problems/lc0102-binary-tree-level-order-traversal/src/solution.rs
@@ -0,0 +1,43 @@
+use super::*;
+
+pub struct Solution;
+
+use std::collections::VecDeque;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+impl Solution {
+    pub fn level_order(mut root: Option<Rc<RefCell<TreeNode>>>) -> Vec<Vec<i32>> {
+        let mut queue = VecDeque::new();
+        let mut out = Vec::new();
+
+        if let Some(root) = root.take() {
+            out.push(vec![]);
+            queue.push_back((root, 1));
+        }
+
+        while let Some((node, level)) = queue.pop_front() {
+            let mut node = node.borrow_mut();
+
+            use std::cmp::Ordering;
+            match out.len().cmp(&level) {
+                Ordering::Equal => {},
+                Ordering::Less => {
+                    out.push(vec![]);
+                },
+                Ordering::Greater => panic!("Queue order violated! [out.len = {}; node.level = {}]", out.len(), level),
+            }
+
+            out.last_mut().expect("Empty `out`").push(node.val);
+
+            if let Some(left) = node.left.take() {
+                queue.push_back((left, level + 1));
+            }
+            if let Some(right) = node.right.take() {
+                queue.push_back((right, level + 1));
+            }
+        }
+
+        out
+    }
+}
