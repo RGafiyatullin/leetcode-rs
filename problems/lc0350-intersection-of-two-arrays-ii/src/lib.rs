0d5adc49a28cb1709be453263e0101bcc81119a3
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-16 00:25:11 +0300

    LC-0350: test large inputs

--- problems/lc0350-intersection-of-two-arrays-ii/src/lib.rs
+++ problems/lc0350-intersection-of-two-arrays-ii/src/lib.rs
@@ -3,7 +3,7 @@ pub mod hashtable;
 
 #[cfg(test)]
 mod tests {
-    use super::hashtable::Solution;
+    use super::array::Solution;
 
     const CASES: &[(&[i32], &[i32], &[i32])] = &[
         (&[1, 2, 2, 1], &[2, 2], &[2, 2]),
@@ -17,6 +17,28 @@ mod tests {
             expected.sort();
 
             let mut out = Solution::intersect(left.to_vec(), right.to_vec());
+            out.sort();
+
+            assert_eq!(out, expected);
+        }
+    }
+
+    fn repeat(input: &[i32], times: usize) -> Vec<i32> {
+        let mut output = Vec::new();
+        for _ in 0..times {
+            output.extend(input);
+        }
+        output
+    }
+
+    #[test]
+    fn large_inputs() {
+        for (left, right, expected) in CASES {
+            const TIMES: usize = 100000;
+            let mut expected = repeat(expected, TIMES);
+            expected.sort();
+
+            let mut out = Solution::intersect(repeat(left, TIMES), repeat(right, TIMES));
             out.sort();
 
             assert_eq!(out, expected);
