e82518eb33ca810cb34857d1514fd8d2820b62ad
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-18 23:59:59 +0300

    lc1074

---
+++ problems/lc1074-number-of-submatrices-that-sum-to-target/src/solution.rs
@@ -0,0 +1,37 @@
+pub struct Solution ;
+
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn num_submatrix_sum_target(matrix: Vec<Vec<i32>>, target: i32) -> i32 {
+        let row_count = matrix.len();
+        let col_count = matrix[0].len();
+
+        let mut prefix_sums = vec![vec![0; col_count + 1]; row_count + 1];
+        for i in 1..=row_count {
+            for j in 1..=col_count {
+                prefix_sums[i][j] = prefix_sums[i - 1][j] + prefix_sums[i][j - 1] - prefix_sums[i - 1][j - 1] + matrix[i - 1][j - 1];
+            }
+        }
+
+        let mut count = 0;
+        
+        let mut h = HashMap::<i32, i32>::new();
+
+        for c1 in 1..=col_count {
+            for c2 in c1..=col_count {
+                h.clear();
+                h.insert(0, 1);
+
+                for row in 1..=row_count {
+                    let curr_sum = prefix_sums[row][c2] - prefix_sums[row][c1 - 1];
+                    let curr_count = *h.get(&(curr_sum - target)).unwrap_or(&0);
+                    count += curr_count;
+                    *h.entry(curr_sum).or_insert(0) += 1;
+                }
+            }
+        }
+
+        count
+    }
+}
---
+++ problems/lc1074-number-of-submatrices-that-sum-to-target/src/tests.rs
@@ -0,0 +1,14 @@
+use crate::Solution;
+
+const CASES: &[(&[&[i32]], i32, i32)] = &[
+    (&[&[0,1,0],&[1,1,1],&[0,1,0]], 0, 4),
+    (&[&[1,-1],&[-1,1]], 0, 5),
+    (&[&[904]], 0, 0),
+];
+
+#[test]
+fn run_all_cases() {
+    for &(matrix, target, expected) in CASES {
+        assert_eq!(Solution::num_submatrix_sum_target(matrix.into_iter().map(|row| row.to_vec()).collect(), target), expected);
+    }
+}
