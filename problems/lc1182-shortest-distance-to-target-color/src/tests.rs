738d3e8a0efc46b63f44a0428a17551c72ccc29b
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-07-20 19:02:43 +0300

    LC1182: Shortest Distance to Target Color

---
+++ problems/lc1182-shortest-distance-to-target-color/src/tests.rs
@@ -0,0 +1,23 @@
+use crate::Solution;
+
+const CASES: &[(&[i32], &[&[i32]], &[i32])] = &[
+    (
+        &[1, 1, 2, 1, 3, 2, 2, 3, 3],
+        &[&[1, 3], &[2, 2], &[6, 1]],
+        &[3, 0, 3],
+    ),
+    (&[1, 2], &[&[0, 3]], &[-1]),
+];
+
+#[test]
+fn run_all_cases() {
+    for &(colors, queries, expected) in CASES {
+        assert_eq!(
+            Solution::shortest_distance_color(
+                colors.to_vec(),
+                queries.into_iter().map(|q| q.to_vec()).collect()
+            ),
+            expected
+        );
+    }
+}
