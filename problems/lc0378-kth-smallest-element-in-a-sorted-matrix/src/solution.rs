c63de0e7def19b0667834ccc9bf99bc526b1b1d7
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-08-02 13:11:13 +0300

    LC-0378: Kth smallest element in a sorted matrix

---
+++ problems/lc0378-kth-smallest-element-in-a-sorted-matrix/src/solution.rs
@@ -0,0 +1,100 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn kth_smallest(matrix: Vec<Vec<i32>>, k: i32) -> i32 {
+        let singles = matrix
+            .iter()
+            .map(IntoIterator::into_iter)
+            .map(Iterator::copied)
+            .map(MergeSort::single)
+            .collect::<Vec<_>>();
+
+        let merge_sorted_opt = {
+            let mut doubles = singles;
+
+            while doubles.len() > 1 {
+                let mut tmp = vec![];
+                while !doubles.is_empty() {
+                    let left = doubles.pop().expect("!doubles.is_empty()");
+                    if let Some(right) = doubles.pop() {
+                        tmp.push(MergeSort::double(left, right));
+                    } else {
+                        tmp.push(left);
+                    }
+                }
+                std::mem::swap(&mut doubles, &mut tmp);
+            }
+
+            doubles.pop()
+        };
+
+        if let Some(merge_sorted) = merge_sorted_opt {
+            merge_sorted
+                .skip(k as usize - 1)
+                .next()
+                .expect("k is too large")
+        } else {
+            panic!("Empty matrix");
+        }
+    }
+}
+
+enum MergeSort<'a> {
+    Single(Option<i32>, Box<dyn Iterator<Item = i32> + 'a>),
+    Double(Box<Self>, Box<Self>),
+}
+
+impl<'a> MergeSort<'a> {
+    pub fn single<I>(inner: I) -> Self
+    where
+        I: IntoIterator<Item = i32> + 'a,
+    {
+        let mut inner = inner.into_iter().fuse();
+        let peeked = inner.next();
+        Self::Single(peeked, Box::new(inner))
+    }
+
+    pub fn double(left: Self, right: Self) -> Self {
+        let left = Box::new(left);
+        let right = Box::new(right);
+
+        Self::Double(left, right)
+    }
+
+    fn peek(&self) -> Option<i32> {
+        match self {
+            Self::Single(peeked, _inner) => *peeked,
+            Self::Double(left, right) => match (left.peek(), right.peek()) {
+                (None, None) => None,
+                (Some(l), None) => Some(l),
+                (None, Some(r)) => Some(r),
+                (Some(l), Some(r)) => Some(std::cmp::min(l, r)),
+            },
+        }
+    }
+}
+
+impl<'a> Iterator for MergeSort<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self {
+            Self::Single(peeked, inner) => {
+                let out = *peeked;
+                *peeked = inner.next();
+
+                out
+            }
+            Self::Double(left, right) => match (left.peek(), right.peek()) {
+                (None, None) => None,
+                (Some(_l), None) => left.next(),
+                (None, Some(_r)) => right.next(),
+                (Some(l), Some(r)) if l <= r => left.next(),
+                (Some(l), Some(r)) => {
+                    assert!(l > r);
+                    right.next()
+                }
+            },
+        }
+    }
+}
--- problems/lc0484-find-permutation/src/solution.rs
+++ problems/lc0484-find-permutation/src/solution.rs
@@ -8,7 +8,7 @@ impl Solution {
 
         let nums_count = input.len() + 1;
         let input_bytes = input.into_bytes();
-        
+
         let mut corrections = vec![0; nums_count];
         for (idx, b) in input_bytes.iter().copied().enumerate().rev() {
             corrections[idx] = corrections.get(idx + 1).copied().unwrap_or_default();
