2000a1a388f7aefb107931d1cab579c72c4c66da
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-08-04 15:34:26 +0300

    LC0729 My Calendar I

---
+++ problems/lc0729-my-calendar-i/src/solution.rs
@@ -0,0 +1,69 @@
+use std::{collections::BTreeSet, cmp::Ordering};
+
+#[derive(Debug, Clone, Default)]
+pub struct MyCalendar(BTreeSet<Interval>);
+
+impl MyCalendar {
+
+    pub fn new() -> Self {
+        Default::default()
+    }
+    
+    pub fn book(&mut self, start_inc: i32, end_exc: i32) -> bool {
+        let requested = Interval { start_inc, end_exc };
+
+        if let Some(_existing) = self.0.get(&requested) {
+            // eprintln!("Conflict [requested: {:?}; existing: {:?}]", requested, _existing);
+            false
+        } else {
+            self.0.insert(requested);
+            true
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub(crate) struct Interval {
+    start_inc: i32,
+    end_exc: i32,
+}
+
+impl Interval {
+    pub fn new(start_inc: i32, end_exc: i32) -> Self {
+        assert!(start_inc < end_exc);
+        Self { start_inc, end_exc }
+    }
+}
+
+
+impl PartialEq for Interval {
+    fn eq(&self, other: &Self) -> bool {
+        self.cmp(other).is_eq()
+    }
+}
+
+impl PartialOrd for Interval {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        let (l_s, l_e) = (self.start_inc, self.end_exc);
+        let (r_s, r_e) = (other.start_inc, other.end_exc);
+
+        assert!(l_s < l_e);
+        assert!(r_s < r_e);
+
+        if r_s >= l_e {
+            Some(Ordering::Less)
+        } else if l_s >= r_e {
+            Some(Ordering::Greater)
+        } else {
+            Some(Ordering::Equal)
+        }
+    }
+}
+
+impl Ord for Interval {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).expect("PartialOrd returned None")
+    }
+}
+
+impl Eq for Interval {}
---
+++ problems/lc0729-my-calendar-i/src/tests.rs
@@ -0,0 +1,85 @@
+
+use crate::{MyCalendar, Interval};
+
+
+#[test]
+fn test_interval_cmp() {
+    assert!(Interval::new(10, 20).cmp(&Interval::new(10, 20)).is_eq());
+    assert!(Interval::new(10, 20).cmp(&Interval::new(15, 25)).is_eq());
+    assert!(Interval::new(15, 25).cmp(&Interval::new(10, 20)).is_eq());
+
+    assert!(Interval::new(10, 20).cmp(&Interval::new(20, 30)).is_lt());
+    assert!(Interval::new(10, 20).cmp(&Interval::new(25, 35)).is_lt());
+
+    assert!(Interval::new(20, 30).cmp(&Interval::new(10, 20)).is_gt());
+    assert!(Interval::new(25, 35).cmp(&Interval::new(10, 20)).is_gt());
+}
+
+
+#[test]
+fn test_00() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        fail(c, 15, 25);
+        ok(c, 20, 30);
+    });
+}
+
+#[test]
+fn test_01_no_conflicts_no_overlaps() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        ok(c, 30, 40);
+    })
+}
+
+#[test]
+fn test_01_no_conflicts_adjacent() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        ok(c, 20, 30);
+    })
+}
+
+#[test]
+fn test_02_start_in_the_middle_of_existing() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        fail(c, 15, 25);
+    })
+}
+
+#[test]
+fn test_03_end_in_the_middle_of_existing() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        fail(c, 5, 15);
+    });
+}
+
+#[test]
+fn test_04_other_start_in_the_middle() {
+    with_calendar(|c| {
+        ok(c, 10, 20);
+        fail(c, 5, 25);
+    });
+}
+
+
+mod utils {
+    use super::*;
+
+    pub(super) fn with_calendar<F>(f: F) where F: FnOnce(&mut MyCalendar) {
+        let mut c = MyCalendar::new();
+        f(&mut c);
+        eprintln!("{:#?}", c);
+    }
+    
+    pub(super) fn ok(c: &mut MyCalendar, start_inc: i32, end_exc: i32) {
+        assert_eq!(c.book(start_inc, end_exc), true);
+    }
+    pub(super) fn fail(c: &mut MyCalendar, start_inc: i32, end_exc: i32) {
+        assert_eq!(c.book(start_inc, end_exc), false);
+    }
+}
+use utils::*;
