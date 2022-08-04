a4744ce9e7cd868d7e22d513fec068c7700a0f0f
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-08-04 20:44:09 +0300

    LC-0367: Valid Perfect Square

--- problems/lc-0367-valid-perfect-square/src/lib.rs
+++ problems/lc-0367-valid-perfect-square/src/lib.rs
@@ -1,8 +1,41 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+
+pub struct Solution;
+
+impl Solution {
+    pub fn is_perfect_square(num: i32) -> bool {
+        let num = num as i64;
+        assert!(num >= 1);
+
+        use std::cmp::Ordering::*;
+
+        let (mut lo, mut hi) = (1, num);
+
+        loop {
+            let mid = (lo + hi) / 2;
+
+            match (lo == hi, (mid * mid).cmp(&num)) {
+                (_, Equal) => break true,
+                (true, _) => break false,
+                (false, Less) => lo = mid + 1,
+                (false, Greater) => hi = mid,
+            }
+        }
     }
+}
+
+#[test]
+fn run_all_cases() {
+    assert!(Solution::is_perfect_square(1));
+    assert!(!Solution::is_perfect_square(2));
+
+    assert!(Solution::is_perfect_square(16));
+    assert!(!Solution::is_perfect_square(14));
+
+    assert!(Solution::is_perfect_square(1024));
+    assert!(Solution::is_perfect_square(1024 * 1024));
+    assert!(Solution::is_perfect_square(1024 * 1024 * 1024));
+
+    assert!(!Solution::is_perfect_square(1024 + 1));
+    assert!(!Solution::is_perfect_square(1024 * 1024 + 1));
+    assert!(!Solution::is_perfect_square(1024 * 1024 * 1024 + 1));
 }
