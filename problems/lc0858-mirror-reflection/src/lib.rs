e346250db29afa282dfb38eb5a6f3ea3801a4765
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-08-04 17:03:41 +0300

    LC-0858 Mirror Reflection

---
+++ problems/lc0858-mirror-reflection/src/lib.rs
@@ -0,0 +1,44 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn mirror_reflection(p: i32, q: i32) -> i32 {
+        let mut pos = 0;
+        let mut left_side = true;
+
+        loop {
+            pos += q;
+            pos %= 2 * p;
+            left_side = !left_side;
+
+            match (pos, left_side) {
+                (0, false) => break 0,
+                (eq_p, false) if eq_p == p => break 1,
+                (eq_p, true) if eq_p == p => break 2,
+                (0, true) => break 3,
+                (_, _) => continue
+            }
+        }
+    }
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const CASES: &[(i32, i32, i32)] = &[
+        (7, 2, 0),
+        (7, 3, 1),
+        (7, 5, 1),
+        (999, 1000, 0),
+    ];
+
+    #[test]
+    fn test() {
+        for &(p, q, exp) in CASES {
+            assert_eq!(Solution::mirror_reflection(p, q), exp);
+        }
+        
+    }
+}
