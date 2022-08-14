0608f040093cb09d7958f8e544ae5b161358e8b6
Raman Hafiyatulin <r.gafiyatullin@me.com>
2022-08-14 20:52:26 +0300

    LC-0126: Works 8)

--- problems/lc0126-word-ladder-ii/src/solution.rs
+++ problems/lc0126-word-ladder-ii/src/solution.rs
@@ -2,15 +2,14 @@ pub struct Solution;
 #[cfg(test)]
 mod tests;
 
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
-use std::rc::Rc;
 
 impl Solution {
     pub fn find_ladders(
         begin_word: String,
         end_word: String,
-        word_list: Vec<String>,
+        mut word_list: Vec<String>,
     ) -> Vec<Vec<String>> {
         assert!(begin_word.len() >= 1);
         assert!(begin_word.len() <= 5);
@@ -18,9 +17,13 @@ impl Solution {
         assert!(word_list.len() >= 1);
         assert!(word_list.len() <= 500);
 
-        let (end_word_idx, bridge_to_words, word_to_bridges) = {
+        let target_word_idx = word_list.len();
+        word_list.push(begin_word);
+        let word_list = word_list;
+
+        let (source_word_idx, bridge_to_words, word_to_bridges) = {
             let mut bridge_to_words = HashMap::<Bridge, Vec<usize>>::new();
-            let mut word_to_bridges = HashMap::<usize, Vec<Bridge>>::new();
+            let mut word_to_bridges: Vec<Vec<Bridge>> = vec![vec![]; word_list.len()];
             let mut end_word_idx = None;
 
             for (idx, word) in word_list.iter().enumerate() {
@@ -30,7 +33,7 @@ impl Solution {
 
                 for bridge in word_bridges(word.as_bytes()) {
                     bridge_to_words.entry(bridge).or_default().push(idx);
-                    word_to_bridges.entry(idx).or_default().push(bridge);
+                    word_to_bridges[idx].push(bridge);
                 }
             }
 
@@ -41,139 +44,103 @@ impl Solution {
             }
         };
 
-        let mut shortest_path_len = Option::<usize>::None;
-        let mut tasks = VecDeque::<Task>::new();
-        let mut visited_words = HashMap::<usize, usize>::new();
-        let mut output = Vec::<Rc<Path>>::new();
+        let mut memo: Vec<(usize, Vec<usize>)> =
+            vec![(word_list.len() + 1, Default::default()); word_list.len()];
+        memo[source_word_idx] = (0, Default::default());
 
-        let path_nil = Rc::new(Path::Nil);
-        for bridge in word_bridges(begin_word.as_bytes()) {
-            tasks.extend(tasks_for_bridge(
-                bridge,
-                &mut visited_words,
-                &bridge_to_words,
-                1,
-                Rc::clone(&path_nil),
-            ));
-        }
+        let mut tasks: VecDeque<_> = vec![source_word_idx].into();
 
-        while let Some(Task {
-            word_idx,
-            path_len,
-            path,
-        }) = tasks.pop_front()
-        {
-            if matches!(shortest_path_len, Some(shortest_path_len) if shortest_path_len < path_len)
-            {
-                continue;
+        let mut best_path_so_far = Option::<usize>::None;
+
+        while let Some(source_word_idx) = tasks.pop_front() {
+            use std::cmp::Ordering::*;
+
+            let (path_len, _) = memo[source_word_idx];
+
+            if best_path_so_far.into_iter().any(|best| path_len >= best) {
+                continue
             }
 
-            if word_idx == end_word_idx {
-                assert!(shortest_path_len.is_none() || shortest_path_len == Some(path_len));
-                shortest_path_len = Some(path_len);
+            for bridge in word_to_bridges[source_word_idx].iter() {
+                for word_idx in bridge_to_words
+                    .get(bridge)
+                    .map(AsRef::<[usize]>::as_ref)
+                    .unwrap_or(&[])
+                    .iter()
+                    .copied()
+                {
+                    if word_list[word_idx] == word_list[source_word_idx]
+                        && word_idx != source_word_idx
+                    {
+                        continue;
+                    }
 
-                output.push(path);
-                continue;
-            }
+                    let (existing_path_len, from_words) = &mut memo[word_idx];
 
-            for bridge in word_to_bridges
-                .get(&word_idx)
-                .map(AsRef::<[Bridge]>::as_ref)
-                .unwrap_or(&[])
-                .iter()
-                .copied()
-            {
-                tasks.extend(tasks_for_bridge(
-                    bridge,
-                    &mut visited_words,
-                    &bridge_to_words,
-                    path_len,
-                    Rc::clone(&path),
-                ))
-            }
-        }
-
-        output
-            .into_iter()
-            .map(|mut path| {
-                let mut words = vec![begin_word.to_owned()];
-
-                loop {
-                    match &*path {
-                        Path::Cons(idx, tail) => {
-                            words.push(word_list[*idx].to_owned());
-                            path = Rc::clone(tail);
+                    let improvement = match (path_len + 1).cmp(existing_path_len) {
+                        Less => {
+                            *existing_path_len = path_len + 1;
+                            from_words.clear();
+                            from_words.push(source_word_idx);
+                            true
                         }
-                        Path::Nil => break,
+                        Equal => {
+                            from_words.push(source_word_idx);
+                            false
+                        }
+                        Greater => false,
+                    };
+                    if word_idx == target_word_idx {
+                        best_path_so_far = Some(path_len + 1);
+                    } else if improvement {
+                        tasks.push_back(word_idx);
                     }
                 }
+            }
+        }
 
-                words[1..].reverse();
+        // eprintln!("BEST-PATH-LEN: {:?}", best_path_so_far);
+        // eprintln!("MEMO");
+        // for (idx, (len, from_words)) in memo.iter().enumerate() {
+        //     eprintln!(
+        //         "- [{:?}] {:?} <- {:?} [{:?}]",
+        //         idx, word_list[idx], from_words, len
+        //     );
+        // }
 
-                // eprintln!("> {:?}", words);
-                words
-            })
-            .collect::<HashSet<_>>()
-            .into_iter()
-            .collect()
+        let mut output = vec![];
+
+        if best_path_so_far.is_some() {
+            let mut tasks = vec![(word_list.len() - 1, vec![])];
+            
+            while let Some((word_idx, mut path)) = tasks.pop() {
+                let (_, next_words) = &memo[word_idx];
+                path.push(word_idx);
+
+                // eprintln!(
+                //     "... COLLECTING [{:?}]{:?} @ {:?}",
+                //     word_idx, word_list[word_idx], path
+                // );
+
+                if next_words.is_empty() {
+                    output.push(
+                        path.into_iter()
+                            .map(|idx| word_list[idx].to_owned())
+                            .collect(),
+                    )
+                } else {
+                    for next_idx in next_words.iter().copied() {
+                        tasks.push((next_idx, path.to_owned()));
+                    }
+                }
+            }
+        }
+        output
     }
 }
 
 fn word_bridges<'a>(word: &'a [u8]) -> impl Iterator<Item = Bridge<'a>> + 'a {
     (0..word.len()).map(move |via| Bridge::new(word, via))
-}
-
-fn tasks_for_bridge<'a, 'b>(
-    bridge: Bridge<'a>,
-    visited_words: &'a mut HashMap<usize, usize>,
-    bridge_to_words: &'a HashMap<Bridge, Vec<usize>>,
-    path_len: usize,
-    path: Rc<Path>,
-) -> impl Iterator<Item = Task> + 'a {
-    bridge_to_words
-        .get(&bridge)
-        .map(AsRef::<[usize]>::as_ref)
-        .unwrap_or(&[])
-        .into_iter()
-        .copied()
-        .filter(move |word_idx| {
-            if let Some(prev_path_len) = visited_words.get(word_idx) {
-                if *prev_path_len < path_len {
-                    false
-                } else {
-                    visited_words.insert(*word_idx, path_len);
-                    true
-                }
-            } else {
-                visited_words.insert(*word_idx, path_len);
-                true
-            }
-        })
-        .map({
-            let path = Rc::clone(&path);
-            move |word_idx| Task {
-                word_idx,
-                path_len: path_len + 1,
-                path: Rc::new(Path::Cons(word_idx, Rc::clone(&path))),
-            }
-        })
-        .inspect(|task| {
-            // std::thread::sleep(std::time::Duration::from_millis(100));
-            // eprintln!("ADDING TASK: {:?}", task)
-        })
-}
-
-#[derive(Debug, Clone)]
-struct Task {
-    word_idx: usize,
-    path_len: usize,
-    path: Rc<Path>,
-}
-
-#[derive(Debug, Clone)]
-enum Path {
-    Nil,
-    Cons(usize, Rc<Self>),
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -190,24 +157,14 @@ impl<'a> Bridge<'a> {
 
 impl<'a> PartialEq for Bridge<'a> {
     fn eq(&self, other: &Self) -> bool {
-        self.word.len() == other.word.len()
-            && self
-                .word
-                .iter()
-                .zip(other.word)
-                .enumerate()
-                .all(|(idx, (l, r))| idx == self.via || *l == *r)
+        (&self.word[0..self.via], &self.word[self.via + 1..])
+            == (&other.word[0..other.via], &other.word[other.via + 1..])
     }
 }
 impl<'a> Eq for Bridge<'a> {}
 
 impl<'a> Hash for Bridge<'a> {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.word.iter().enumerate().for_each(|(idx, letter)| {
-            if idx != self.via {
-                state.write_usize(idx);
-                state.write_u8(*letter);
-            }
-        });
+        (&self.word[0..self.via], &self.word[self.via + 1..]).hash(state)
     }
 }
